23d1abe76d450ebb1db0379b8ccc0b72ed0f6750
w-ziem <143935791+w-ziem@users.noreply.github.com>
2024-12-13 13:23:56 +0100

    Fix comparing floating point numbers error

--- src/backsubst.c
+++ src/backsubst.c
@@ -1,7 +1,7 @@
 #include "backsubst.h"
 #include <math.h>
 #include <stdio.h>
-
+#define EPSILON 1e-10
 /**
  * Zwraca 0 - wsteczne podstawienie zakonczone sukcesem
  * Zwraca 1 - błąd dzielenia przez 0 (element na diagonali = 0)
@@ -21,7 +21,7 @@ int  backsubst(Matrix *x, Matrix *mat, Matrix *b) {
 
 		// Sprawdzenie, czy element na diagonali jest równy 0
 		//element nie będzie równy dokładnie zero nigdy, ponieważ działania na liczbach zmiennoprzecinkowych są obarczone błędem
-		if (fabs(mat->data[i][i]) == 0) {
+		if (fabs(mat->data[i][i])  < EPSILON) {
 			return 1;
 		}
 
